feb86bed8192bb2c769110e4f8ea3790d1f31d08
OtavioRL <otaviohlopess@gmail.com>
2024-01-09 17:39:50 -0300

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
 
       <CyberApCanvas />
 
-      <div className=' absolute xs:bottom-10 bottom-31 w-full flex justify-center items-center'>
+      <div className=' absolute xs:bottom-10 bottom-20 w-full flex justify-center items-center'>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.dev 
